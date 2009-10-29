6e8f05a1a8dcf58f5eb66e7b3a862afb3f367f6e
Wayne E. Seguin <wayneeseguin@gmail.com>
2009-10-28 20:03:13 -0400

    Patches: Thank you Thomas Ritz. Altered irb files to use env vars.

--- scripts/irbrc.rb
+++ scripts/irbrc.rb
@@ -6,17 +6,17 @@ require "irb/completion" rescue nil
 
 # Turn on history saving.
 require "irb/ext/save-history"
-IRB.conf[:HISTORY_FILE] = File.join(ENV['HOME'], ".irb-history")
+IRB.conf[:HISTORY_FILE] = File.join(ENV["HOME"], ".irb-history")
 
-# Calculate the package name.
-RVM_RUBY_PACKAGE_NAME rescue RVM_RUBY_PACKAGE_NAME="ruby-#{RUBY_VERSION}"
+# Calculate the ruby string.
+rvm_ruby_string = ENV["rvm_ruby_string"] || "#{RUBY_ENGINE}-#{RUBY_VERSION}-#{(RUBY_PATCHLEVEL) ? "p#{RUBY_PATCHLEVEL}" : "r#{RUBY_REVISION}"}"
 
 # Set up the prompt to be RVM specific.
 @prompt = {
-  :PROMPT_I => "#{RVM_RUBY_PACKAGE_NAME} > ",  # default prompt
-  :PROMPT_S => "#{RVM_RUBY_PACKAGE_NAME}%l> ", # known continuation
-  :PROMPT_C => "#{RVM_RUBY_PACKAGE_NAME} > ",
-  :PROMPT_N => "#{RVM_RUBY_PACKAGE_NAME} ?> ", # unknown continuation
+  :PROMPT_I => "#{rvm_ruby_string} > ",  # default prompt
+  :PROMPT_S => "#{rvm_ruby_string}%l> ", # known continuation
+  :PROMPT_C => "#{rvm_ruby_string} > ",
+  :PROMPT_N => "#{rvm_ruby_string} ?> ", # unknown continuation
   :RETURN => " => %s \n",
   :AUTO_INDENT => true
 }
@@ -24,6 +24,8 @@ IRB.conf[:PROMPT][:RVM] = @prompt
 IRB.conf[:PROMPT_MODE] = :RVM
 
 # Load the user's irbrc file, if possible.
-load File.join(ENV['HOME'], '.irbrc') rescue nil
+if File.exists?("#{ENV["HOME"]}/.irbrc_disabled")
+  load File.join(ENV["HOME"], ".irbrc") rescue nil
+end
 
 # EOF
