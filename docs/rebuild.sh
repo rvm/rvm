55a88067ff87b62c2a89ee62cd9c60b22a1104db
Darcy Laycock <sutto@sutto.net>
2010-06-23 08:00:57 +0800

    [WIP] Work on tweaking the build process for docs

--- docs/rebuild.sh
+++ docs/rebuild.sh
@@ -22,40 +22,41 @@ mkdir -p ${rvm_docs_target_man_dir}
 echo "Starting doc generation run through."
 
 # processing manpages
-find ${rvm_docs_src_dir} -type f -name *.txt | while read i; do
-    _manpage_file="${i}"
+find ${rvm_docs_src_dir} -type f -name *.txt | while read rvm_manpage_file; do
 
     # trying to detect manpage name automatically
-    # (just for fun, I don't think, that rvm will ever have
-    # moar then one manpage :)
-    # The name of the generated manpage is initially specified
-    # within the source file in asciidoc format,
+    # (just for fun, I don't think, that rvm will ever have more then one manpage :)
+    # The name of the generated manpage is initially specified within the source file in asciidoc format,
     # so we'll do some simple parsing
-    # We assume, that it will be specified at one of the 3(three) first lines
+    # We assume, that it will be specified at one of the 3 (three) first lines
     # of the source file.
 
-    # it should be smth like 'RVM(1)'
-    _manpage_name_full="$(cat ${_manpage_file} | head -n 3 | grep -o '^[^(]*[(][^)]*[)]$')"
-    [[ -z "${_manpage_name_full}" ]] && (echo "Unable to detect manpage name, stopping build process..."; exit 1)
-    # we need smth like 'rvm.1'
-    _manpage_name=$(echo ${_manpage_name_full} | sed "s|(|.|;s|)||" | tr '[[:upper:]]' '[[:lower:]]')
-    # we need '1'
-    _manpage_name_part=$(echo ${_manpage_name} | cut -d '.' -f 2)
-    # So, the manpage directory will be the following:
-    _manpage_dir=${rvm_docs_target_man_dir}/man${_manpage_name_part}
-    mkdir -p ${_manpage_dir}
+    # it should be something like 'RVM(1)'
+    rvm_manpage_name_full="$(cat ${rvm_manpage_file} | head -n 3 | grep -o '^[^(]*[(][^)]*[)]$')"
 
-    echo "Generating docbook format from source file for $_manpage_name"
-    asciidoc -d manpage -b docbook -o "${rvm_tmp_dir}/${_manpage_name}.dbk" ${_manpage_file}
-    pushd "$rvm_tmp_dir" >/dev/null 2>&1
-    echo "Generating manpage from docbook file."
-    docbook2man "${rvm_tmp_dir}/${_manpage_name}.dbk"
-    popd >/dev/null 2>&1
+    if [[ -z "${rvm_manpage_name_full}" ]]; then
+      echo "Unable to detect manpage name, stopping build process..." 1>&2
+      exit 1
+    fi
+
+    # we need smth like 'rvm.1'
+    rvm_manpage_name="$(echo "$rvm_manpage_name_full" | sed "s|(|.|;s|)||" | tr '[[:upper:]]' '[[:lower:]]')"
+    # we need '1'
+    rvm_manpage_name_part=$(echo "$rvm_manpage_name" | cut -d '.' -f 2)
+    # So, the manpage directory will be the following:
+    rvm_manpage_dir="$rvm_docs_target_man_dir/man$rvm_manpage_name_part"
+    mkdir -p "$rvm_manpage_dir"
+
+    echo "Generating docbook format from source file for $rvm_manpage_name"
+    asciidoc -d manpage -b docbook -o "$rvm_tmp_dir/${rvm_manpage_name}.dbk" "$rvm_manpage_file"
+
+    echo "Generating manpage from docbook"
+
     echo "Moving manpage to the the correct folder"
-    mv "${rvm_tmp_dir}/${_manpage_name}" "${_manpage_dir}"
+    mv "$rvm_tmp_dir/$rvm_manpage_name" "$rvm_manpage_dir"
     # compression is optional, but gzip check added for neatness
     echo "If gzip is available, compressing"
-    [[ `which gzip` ]] && gzip -f "${_manpage_dir}/${_manpage_name}"
+    [[ `which gzip` ]] && gzip -f "$rvm_manpage_dir/$rvm_manpage_name"
 done
 
 # vim: ft=sh
