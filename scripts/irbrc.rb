461be7d901d117a179e9770f2c10c4c681e4f122
Wayne E. Seguin <wayneeseguin@gmail.com>
2010-09-16 20:38:54 -0400

    Do not load completion if readline failed.

--- scripts/irbrc.rb
+++ scripts/irbrc.rb
@@ -3,34 +3,37 @@
 
 # Turn on completion.
 begin
-require "readline"
+  require "readline"
+
+  require "irb/completion" rescue nil
+
+  # Turn on history saving.
+  # require "irb/ext/save-history"
+  # IRB.conf[:HISTORY_FILE] = File.join(ENV["HOME"], ".irb-history")
+
+  # Use an alternate way to on history saving until save-history is fixed.
+  #
+  #   bug:   http://redmine.ruby-lang.org/issues/show/1556
+  #   patch: http://pastie.org/513500
+  #
+  # This technique was adopted from /etc/irbrc on OS X.
+  histfile = File::expand_path(".irb-history", ENV["HOME"])
+
+  if File::exists?(histfile)
+    lines = IO::readlines(histfile).collect { |line| line.chomp }
+    Readline::HISTORY.push(*lines)
+  end
+
+  Kernel::at_exit do
+    maxhistsize = 100
+    histfile = File::expand_path(".irb-history", ENV["HOME"])
+    lines = Readline::HISTORY.to_a.reverse.uniq.reverse
+    lines = lines[-maxhistsize, maxhistsize] if lines.nitems > maxhistsize
+    File::open(histfile, File::WRONLY|File::CREAT|File::TRUNC) { |io| io.puts lines.join("\n") }
+  end
+
 rescue
   puts "Readline was unable to be required, if you need completion or history install readline then reinstall the ruby.\nYou may follow 'rvm notes' for dependencies and/or read the docs page http://rvm.beginrescueend.com/packages/readline/ . Be sure you 'rvm remove X ; rvm install X' to re-compile your ruby with readline support after obtaining the readline libraries."
-end
-require "irb/completion" rescue nil
-
-# Turn on history saving.
-# require "irb/ext/save-history"
-# IRB.conf[:HISTORY_FILE] = File.join(ENV["HOME"], ".irb-history")
-
-# Use an alternate way to on history saving until save-history is fixed.
-#
-#   bug:   http://redmine.ruby-lang.org/issues/show/1556
-#   patch: http://pastie.org/513500
-#
-# This technique was adopted from /etc/irbrc on OS X.
-histfile = File::expand_path(".irb-history", ENV["HOME"])
-
-if File::exists?(histfile)
-  lines = IO::readlines(histfile).collect { |line| line.chomp }
-  Readline::HISTORY.push(*lines)
-end
-
-Kernel::at_exit do
-  maxhistsize = 100
-  lines = Readline::HISTORY.to_a.reverse.uniq.reverse
-  lines = lines[-maxhistsize, maxhistsize] if lines.nitems > maxhistsize
-  File::open(histfile, File::WRONLY|File::CREAT|File::TRUNC) { |io| io.puts lines.join("\n") }
 end
 
 # Calculate the ruby string.
@@ -57,3 +60,4 @@ rescue LoadError => load_error
 rescue => exception
   puts "Error : 'load #{ENV["HOME"]}/.irbrc' : #{exception.message}"
 end
+
