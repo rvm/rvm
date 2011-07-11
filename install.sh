d551a5455a6a3619a49e8bf84dc98b37b018e0ca
Stefan Kaes <skaes@railsexpress.de>
2011-07-11 14:02:18 +0200

    made patches ruby version specific

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find $rvm_path -name '*railsexpress*' | xargs rm -rf
+find $rvm_path/patches $rvm_path/patches -name '*railsexpress*' | xargs rm -rf
 cp -rp patches patchsets $rvm_path
 for i in "$@"; do
   rvm install $i --force --patch railsexpress
