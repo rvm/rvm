97705d42be80f6b962b7840be4ead4d82fee40b7
Deryl R. Doucette <me@deryldoucette.com>
2011-10-15 22:29:58 -0400

    Moved the scripts under bin/ I now have Connection, Execution, Data Storage, Reporting, and Deletion

--- test/rvm_test_suite/test_generation/ruby/bin/run.rb
+++ test/rvm_test_suite/test_generation/ruby/bin/run.rb
@@ -1,17 +1,45 @@
+# Populate @current_dir so we don't have to always write out the full path
+# NOTE: We need a better way of doing this
 @current_dir = `PWD`
 
+# Connect to the database using ActiveRecord
 load "#{@current_dir.strip}/bin/connect.rb"
+
+# Now load the Model(s)
 load "#{@current_dir.strip}/app/models/commands.rb"
 
+# Create a new Command object
 @command = Command.new
+
+# Define the system man and its OS
 @command.sysname = `uname -n`.strip
 @command.os_type = `uname -s`.strip
+
+# Now, we passed the command to run, so lets store it in .cmd
 @command.cmd = ARGV[0]
-@command.cmd_output = `rvm info`
+
+# Now we execute the command and trap its output
+@command.cmd_output =  `#{@command.cmd}`
+
+# And now we save it to the database
 @command.save!
 
-puts "\t\t\t\t*************** [ SYSTEM REPORT FOR #{@command.sysname} ] ***************\t\t\t\t\n\n"
+# Now we artistically display a report of everything
+puts "\t\t\t\t*************** [ TESTING REPORT FOR #{@command.sysname} ] ***************\t\t\t\t\n\n"
 puts " COMMAND ID #: #{@command.id}\n SYSTEM TYPE: #{@command.os_type}\n EXECUTED COMMAND: #{@command.cmd}\n COMMAND OUTPUT: #{@command.cmd_output}"
 
-0.upto(@command.id - 1) {|id| puts "Deleting Record #" + @command.id.to_s ; @command[id].destroy }
+# Now lets find all the previously stored runs
+@commands = Command.find :all
 
+# Next, we sort the commands on the ID field so it displays right
+@commands.sort! { |old,cur| old.id <=> cur.id }
+
+# Now, for each of those, display what they did, then delete them.
+# TODO Optmize this with custom SQL in the future
+@commands.each do |command|
+  unless command.id == @command.id
+    puts "SYSTEM: " + "#{command.sysname} - " + "Previous cmd ID: " + command.id.to_s + " - Executed: \"#{command.cmd.to_s}\"" + " at " +  "#{command.updated_at.to_s}"
+    # Eventually, we'll separate this information and the actual delete. Leaving here while testing.
+    #command.destroy
+  end
+end
