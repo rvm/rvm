780819073d1afb0bb2beca11d70c457893ef31be
Deryl R. Doucette <me@deryldoucette.com>
2011-10-18 19:06:50 -0400

    Forgot to populate the single command execution with the timings code as well. Fixed. but the problems from the (literally) previous commit still stand.

--- test/rvm_test_suite/ruby/bin/run.rb
+++ test/rvm_test_suite/ruby/bin/run.rb
@@ -107,18 +107,27 @@ else
   # All is good so onwards and upwards! This handles when just a single command,
   # not a script, is passed
   @command = Command.new("cmd" => ARGV[0])
-  # Now we execute the command and trap its output, but don't display it on the screen.
-  # We'll show it later on, in the report.
-  @command.cmd_output =  %x[#{@command.cmd} 2>&1]
 
   # Capture the system's name and its OS
   @command.sysname = %x[uname -n].strip
   @command.os_type = %x[uname -s].strip
 
-  # TODO - Add test timings here as well for the singular command capability as well.
+  # Generate the timings. This is done by passing in the block to be executed which includes storing the
+  # output in @commands.cmd_output, then recording the returned timings generated within the record_timings call
+  # into @test-report.timings. and save the report.
+  @test_report.timings = @test_report.record_timings { @command.cmd_output = %x[#{@command.cmd} 2>&1] }
+  @test_report.save!
+
+  # now that @test_report has been saved and an ID generated, associate that test report with @command
+  @command.test_report = @test_report
+  # Now save @command to generate its own ID, and then associate that with @test_report and save the association
+  # to @test_report
+  @command.save!
+  @test_report.commands << @command
   
   # And now we save it all to the database.
   @command.save!
+  @test_report.save!          
 
 end
 
