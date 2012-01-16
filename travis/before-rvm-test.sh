29cb4607d9e9c7fe18b6791f61c9b0c0312eb4f5
Michal Papis <mpapis@gmail.com>
2012-01-16 21:50:30 +0100

    trace rake to find errors

--- travis/before-rvm-test.sh
+++ travis/before-rvm-test.sh
@@ -7,5 +7,5 @@ cd rvm-test/
 cp example_configs/db/sqlite_config.yml       db/config.yml
 cp example_configs/config/sqlite_database.yml config/database.yml
 bundle install --without long
-rake db:create db:migrate
+rake --trace db:create db:migrate
 cd -
