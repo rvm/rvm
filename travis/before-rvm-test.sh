10f0e36ed573ed69a2f8c697d9e68ef217936cb8
Michal Papis <mpapis@gmail.com>
2012-01-16 21:03:38 +0100

    update for travis

--- travis/before-rvm-test.sh
+++ travis/before-rvm-test.sh
@@ -2,8 +2,11 @@
 
 ./install
 (
+  git submodules init
+  git submodules update
   cd rvm-test/
   cp example_configs/db/sqlite_config.yml       db/config.yml
   cp example_configs/config/sqlite_database.yml config/database.yml
+  bundle install --without long
   rake db:create db:migrate
 )
