35922e29dcdb7535ae246208f7ff1c6901b19b1d
Michal Papis <mpapis@gmail.com>
2012-01-16 21:07:03 +0100

    fail early in travis/before_install

--- travis/before-rvm-test.sh
+++ travis/before-rvm-test.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 ./install
-(
-  git submodules init
-  git submodules update
-  cd rvm-test/
-  cp example_configs/db/sqlite_config.yml       db/config.yml
-  cp example_configs/config/sqlite_database.yml config/database.yml
-  bundle install --without long
-  rake db:create db:migrate
-)
+git submodule init
+git submodule update
+cd rvm-test/
+cp example_configs/db/sqlite_config.yml       db/config.yml
+cp example_configs/config/sqlite_database.yml config/database.yml
+bundle install --without long
+rake db:create db:migrate
+cd -
