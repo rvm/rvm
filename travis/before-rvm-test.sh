83c3a71485440d4fd4562aac2b41a3e3156031aa
Michal Papis <mpapis@gmail.com>
2012-01-18 15:41:33 +0100

    update travis scripts

--- travis/before-rvm-test.sh
+++ travis/before-rvm-test.sh
@@ -7,5 +7,5 @@ cd rvm-test/
 cp example_configs/db/sqlite_config.yml       db/config.yml
 cp example_configs/config/sqlite_database.yml config/database.yml
 bundle install --without long
-rake --trace db:create db:migrate
+rake db:create db:migrate
 cd -
--- travis/rvm-test.sh
+++ travis/rvm-test.sh
@@ -2,3 +2,4 @@
 
 cd rvm-test
 env time -f "# took: %E" ruby bin/run.rb --script batch_scripts/fast --short
+cd -
