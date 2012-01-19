8b5fb14d54ef7d450d46bbc131a3d3729965a109
Michal Papis <mpapis@gmail.com>
2012-01-19 03:48:36 +0100

    test gemsets cleanup on delete

--- travis/rvm-test.sh
+++ travis/rvm-test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd rvm-test
-env time -f "# took: %E" ruby bin/run.rb --script batch_scripts/fast --short
+env time -f "# took: %E" ruby bin/run.rb --script batch_scripts/fast/* --short
 cd -
