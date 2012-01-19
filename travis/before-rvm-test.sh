cba43292154380874652af43ead3eb87e5ef1290
Michal Papis <mpapis@gmail.com>
2012-01-19 03:56:04 +0100

    fix return status for tests

--- travis/before-rvm-test.sh
+++ travis/before-rvm-test.sh
@@ -3,9 +3,10 @@
 ./install
 git submodule init
 git submodule update
-cd rvm-test/
-cp example_configs/db/sqlite_config.yml       db/config.yml
-cp example_configs/config/sqlite_database.yml config/database.yml
-bundle install --without long
-rake db:create db:migrate
-cd -
+(
+  cd rvm-test/
+  cp example_configs/db/sqlite_config.yml       db/config.yml
+  cp example_configs/config/sqlite_database.yml config/database.yml
+  bundle install --without long
+  rake db:create db:migrate
+)
--- travis/rvm-test.sh
+++ travis/rvm-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-cd rvm-test
-env time -f "# took: %E" ruby bin/run.rb --script batch_scripts/fast/* --short
-cd -
+(
+  cd rvm-test
+  env time -f "# took: %E" ruby bin/run.rb --script batch_scripts/fast/* --short
+)
