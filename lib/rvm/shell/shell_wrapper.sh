9e29b70d0192aac872272dd77026ff0d29ef3bfd
Michal Papis <mpapis@gmail.com>
2012-03-27 01:08:30 +0200

    rvm lib was extracted to rvm-gem and rvm-capistrano

--- lib/rvm/shell/shell_wrapper.sh
+++
@@ -1,9 +0,0 @@
-# Prints an epilog to a shell command.
-__rvm_show_command_epilog() {
-  local _code="$?"
-  echo "---------------RVM-RESULTS-START---------------"
-  ruby -rrubygems -ryaml -e \
-      "puts YAML.dump({'environment' => ENV.to_hash, 'exit_status' => '${_code}'})"
-  echo "----------------RVM-RESULTS-END----------------"
-}
-
