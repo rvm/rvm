e343a01b5042ba831d70ea7b72a0461e1e807fa7
Michal Papis <mpapis@gmail.com>
2012-03-27 19:35:57 +0200

    update capistrano error to show change in :rvm_type

--- lib/rvm/capistrano.rb
+++ lib/rvm/capistrano.rb
@@ -3,4 +3,5 @@
 raise "RVM - Capistrano integration was extracted to a separate gem, \
 install: `gem install rvm-capistrano`, \
 remove the `$LOAD_PATH.unshift` line, \
-change require line to: `require 'rvm-capistrano'`."
+change require line to: `require 'rvm-capistrano'`, \
+note also the 'set :rvm_type, :user' is now the default (instead of :system)."
