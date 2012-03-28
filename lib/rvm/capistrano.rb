d6449a7acf60b29baef5dab9bca0005ff923ce8c
Michal Papis <mpapis@gmail.com>
2012-03-28 21:23:25 +0200

    update rvm-capistrano error

--- lib/rvm/capistrano.rb
+++ lib/rvm/capistrano.rb
@@ -1,7 +1,5 @@
 # Recipes for using RVM on a server with capistrano.
 
 raise "RVM - Capistrano integration was extracted to a separate gem, \
-install: `gem install rvm-capistrano`, \
-remove the `$LOAD_PATH.unshift` line, \
-change require line to: `require 'rvm-capistrano'`, \
+install: `gem install rvm-capistrano` and remove the `$LOAD_PATH.unshift` line, \
 note also the 'set :rvm_type, :user' is now the default (instead of :system)."
