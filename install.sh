f33282afe18b691814603f2d697850883d24782d
Stefan Kaes <skaes@railsexpress.de>
2012-04-01 13:47:42 +0200

    use reinstall instead of install --force

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 find $rvm_path/patches $rvm_path/patchsets -name '*railsexpress*' | xargs rm -rf
 cp -rp patches patchsets $rvm_path
 for i in "$@"; do
-  rvm install $i --force --patch railsexpress
+  rvm reinstall $i --patch railsexpress
 done
