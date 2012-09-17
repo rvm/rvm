db8171526e741c77e02cd5af3581625eaa7d426d
Stefan Kaes <skaes@railsexpress.de>
2012-09-17 16:44:58 +0200

    added 1.8.7 patch level 370 patch set

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 find $rvm_path/patches $rvm_path/patchsets -name '*railsexpress*' | xargs rm -rf
 cp -rp patches patchsets $rvm_path
+
+if [[ "`uname -a`" =~ (Darwin Kernel Version 12) ]]; then
+  export CPPFLAGS=-I/opt/X11/include
+fi
+
 for i in "$@"; do
   rvm reinstall $i --patch railsexpress
 done
