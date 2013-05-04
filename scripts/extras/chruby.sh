11a7e96d76986c9b7a10814cb68cc57ada2f3f68
Michal Papis <mpapis@gmail.com>
2013-05-04 05:10:48 +0200

    add scripts/extras/chruby.sh - mrvm - special version for chruby

---
+++ scripts/extras/chruby.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Install rubies via rvm and reload RUBIES
+function mrvm()
+{
+  typeset __result=0
+  if
+    [[ -z "$rvm_path" ]]
+  then
+    typeset reset_rvm_path=true
+    if [[ -x "$HOME/.rvm/bin/rvm" ]]
+    then export rvm_path="$HOME/.rvm"
+    elif [[ -x "/usr/local/rvm/bin/rvm" ]]
+    then export rvm_path="/usr/local/rvm"
+    else return 127
+    fi
+  fi
+  "$rvm_path/bin/rvm" "$@" || __result=$?
+  RUBIES=($( ls -1d "$rvm_path/rubies"/* 2>/dev/null ))
+  if [[ -n "${reset_rvm_path:-}" ]]
+  then unset rvm_path
+  fi
+  return ${__result}
+}
