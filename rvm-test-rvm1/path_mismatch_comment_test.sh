79352b727b6550f9ac9283019a7578991eb14164
Michal Papis <mpapis@gmail.com>
2013-07-22 23:38:17 +0200

    add tests for GEM_HOME / PATH mismatch problems, update #2050

---
+++ rvm-test-rvm1/path_mismatch_comment_test.sh
@@ -0,0 +1,17 @@
+source "$rvm_path/scripts/rvm"
+
+: test no error
+rvm use 1.9.3 --install   # status=0
+rvm version
+# match!=/is not at first place/
+# match!=/is not available/
+
+: test GEM_HOME second
+rvm use 1.9.3             # status=0
+GEM_HOME=$GEM_HOME@global
+rvm version               # match=/is not at first place/
+
+: test GEM_HOME missing
+rvm use 1.9.3             # status=0
+GEM_HOME=$GEM_HOME@veve
+rvm version               # match=/is not available/
