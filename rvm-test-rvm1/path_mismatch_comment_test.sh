6614ca4d94e6854fab3d2d8999e74be9b42b56b8
Michal Papis <mpapis@gmail.com>
2013-07-26 02:36:19 +0200

    add test for cd .rvmrc GEM_HOME warning, fix #2050

--- rvm-test-rvm1/path_mismatch_comment_test.sh
+++ rvm-test-rvm1/path_mismatch_comment_test.sh
@@ -22,3 +22,19 @@ GEM_HOME=$GEM_HOME@veve
 rvm-shell 1.9.3 -c 'true'
 # match!=/is not at first place/
 # match!=/is not available/
+
+: cd + .rvmrc
+rvm use 1.9.3
+true TMPDIR:${TMPDIR:=/tmp}:
+d=$TMPDIR/test-cd-rvmrc
+mkdir -p $d
+echo "rvm use 1.9.3@veve --create" >> $d/.rvmrc
+rvm rvmrc trust $d/.rvmrc
+
+cd $d
+# match!=/is not at first place/
+# match!=/is not available/
+# env[GEM_HOME]=/@veve$/
+
+rvm rvmrc reset $d/.rvmrc
+rm -rf $d
