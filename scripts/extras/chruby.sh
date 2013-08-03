6345e4a7b2a3e1791ad01baf462783c31611899e
Michal Papis <mpapis@gmail.com>
2013-08-03 02:59:36 +0200

    improved detection of rvm_path, fix #2058

--- scripts/extras/chruby.sh
+++ scripts/extras/chruby.sh
@@ -4,11 +4,16 @@
 function mrvm()
 {
   typeset __result=0
+  # duplication marker jdgkjnfnkjdngjkfnd4fd
   if
     [[ -z "$rvm_path" ]]
   then
     typeset reset_rvm_path=true
-    if [[ -x "$HOME/.rvm/bin/rvm" ]]
+    if [[ -n "${BASH_SOURCE:-$0}" && -f "${BASH_SOURCE:-$0}" ]]
+    then
+      export rvm_path="${BASH_SOURCE:-$0}"
+      rvm_path="${rvm_path%/scripts/initialize}"
+    elif [[ -x "$HOME/.rvm/bin/rvm" ]]
     then export rvm_path="$HOME/.rvm"
     elif [[ -x "/usr/local/rvm/bin/rvm" ]]
     then export rvm_path="/usr/local/rvm"
@@ -18,7 +23,7 @@ function mrvm()
   "$rvm_path/bin/rvm" "$@" || __result=$?
   RUBIES=($( ls -1d "$rvm_path/rubies"/* 2>/dev/null ))
   if [[ -n "${reset_rvm_path:-}" ]]
-  then unset rvm_path
+  then unset rvm_path reset_rvm_path
   fi
   return ${__result}
 }
