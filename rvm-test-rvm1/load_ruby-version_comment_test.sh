7f8675a09d53afa090270d39eb1339810b9b5959
Michal Papis <mpapis@gmail.com>
2014-02-28 09:01:29 +0100

    fix version tests, update wayneeseguin/rvm-test#22

--- rvm-test-rvm1/load_ruby-version_comment_test.sh
+++ rvm-test-rvm1/load_ruby-version_comment_test.sh
@@ -10,7 +10,7 @@ PATH="$( echo $PATH | sed 's/^.*rvm[^:]*://' )" # env[PATH]!=/rvm/
 "$rvm_path/bin/rvm" alias delete default # status=0
 "$rvm_path/bin/rvm" alias create default 1.9.3-p448 # status=0
 
-echo "2.0.0" > .ruby-version
+echo "2.0.0-p353" > .ruby-version
 echo "@test" > .ruby-gemset
 source "$rvm_path/scripts/rvm"
 # env[GEM_HOME]=/2.0.0-p353@test$/
