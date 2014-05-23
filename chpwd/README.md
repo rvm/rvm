358b6755810b0d5e5828af2edd89c01511e28437
Michal Papis <mpapis@gmail.com>
2014-05-24 00:26:54 +0200

    extract loading to separate file - planning to test the hook

--- chpwd/README.md
+++ chpwd/README.md
@@ -5,13 +5,13 @@ http://zsh.sourceforge.net/Doc/Release/Functions.html#Hook-Functions
 
 ## Usage
 
-1. execute content of `function.sh`, eg:
+1. load `function.sh` and `load.sh`, eg:
 
         source chpwd/functions.sh
+        source chpwd/load.sh
 
 2. add the hook - replace `_hook_name` with your function name:
 
         export -a chpwd_functions                              # define hooks as an shell array
         [[ " ${chpwd_functions[*]} " == *" _hook_name "* ]] || # prevent double addition
         chpwd_functions=( "${chpwd_functions[@]}" _hook_name ) # finally add it to the list
-
--- chpwd/function.sh
+++ chpwd/function.sh
@@ -1,25 +1,18 @@
-[[ -n "${ZSH_VERSION:-}" ]] ||
+__zsh_like_cd()
 {
-  # On bash and old zsh we need to define chpwd_functions handler
-  __zsh_like_cd()
-  {
-    typeset __zsh_like_cd_hook
-    if
-      builtin "$@"
-    then
-      shift || true # remove the called method
-      for __zsh_like_cd_hook in chpwd "${chpwd_functions[@]}"
-      do
-        if typeset -v "$__zsh_like_cd_hook" >/dev/null 2>&1
-        then "$__zsh_like_cd_hook" "$@" || break # finish on first failed hook
-        fi
-      done
-      true
-    else
-      return $?
-    fi
-  }
-  cd()    { __zsh_like_cd cd    "$@" ; }
-  popd()  { __zsh_like_cd popd  "$@" ; }
-  pushd() { __zsh_like_cd pushd "$@" ; }
+  typeset __zsh_like_cd_hook
+  if
+    builtin "$@"
+  then
+    shift || true # remove the called method
+    for __zsh_like_cd_hook in chpwd "${chpwd_functions[@]}"
+    do
+      if typeset -v "$__zsh_like_cd_hook" >/dev/null 2>&1
+      then "$__zsh_like_cd_hook" "$@" || break # finish on first failed hook
+      fi
+    done
+    true
+  else
+    return $?
+  fi
 }
