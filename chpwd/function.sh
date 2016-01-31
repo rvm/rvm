68fd2bad434696a024ddea1eeff9291269998ab3
Michal Papis <mpapis@gmail.com>
2016-01-31 12:12:20 +0100

    escape typeset's

--- chpwd/function.sh
+++ chpwd/function.sh
@@ -1,12 +1,12 @@
 __zsh_like_cd()
 {
-  typeset __zsh_like_cd_hook
+  \typeset __zsh_like_cd_hook
   if
     builtin "$@"
   then
     for __zsh_like_cd_hook in chpwd "${chpwd_functions[@]}"
     do
-      if typeset -f "$__zsh_like_cd_hook" >/dev/null 2>&1
+      if \typeset -f "$__zsh_like_cd_hook" >/dev/null 2>&1
       then "$__zsh_like_cd_hook" || break # finish on first failed hook
       fi
     done
