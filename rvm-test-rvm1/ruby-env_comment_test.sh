3e12bb9b40e8323193de78e0f14064d08c4e027d
Michal Papis <mpapis@gmail.com>
2017-01-21 18:32:36 +0100

    start asking to trust files with environment variables, do not bundle install if no rvm_autoinstall_bundler_flag=1

--- rvm-test-rvm1/ruby-env_comment_test.sh
+++ rvm-test-rvm1/ruby-env_comment_test.sh
@@ -7,33 +7,36 @@ true TMPDIR:${TMPDIR:=/tmp}:
 d=$TMPDIR/test-ruby-env
 mkdir -p $d
 rvm use --install 2.1.1
-typeset -a __variables rvm_saved_env
+typeset -a __file_env_variables rvm_saved_env
+
+### test with disabled trusting
+export rvm_trust_rvmrcs_flag=1
 
 : simple file loading
 printf "a\nb\n" > $d/.env
-__rvm_project_ruby_env_load_parse_file $d/.env ""
-# env[__variables][]=2
-# env[__variables][0]=/^a$/
-# env[__variables][1]=/^b$/
+__rvm_file_load_env $d/.env ""
+# env[__file_env_variables][]=2
+# env[__file_env_variables][0]=/^a$/
+# env[__file_env_variables][1]=/^b$/
 
 : prefixed file loading
 printf "env-c\nenv-d\n" > $d/.env
-__rvm_project_ruby_env_load_parse_file $d/.env "env-"
-# env[__variables][]=2
-# env[__variables][0]=/^c$/
-# env[__variables][1]=/^d$/
+__rvm_file_load_env $d/.env "env-"
+# env[__file_env_variables][]=2
+# env[__file_env_variables][0]=/^c$/
+# env[__file_env_variables][1]=/^d$/
 
 : remove empty new lines and comments
 printf "d\n\n\043e\nf\n" > $d/.env    ## \043 for # as tf has a parsing bug
-__rvm_project_ruby_env_load_parse_file $d/.env ""
-# env[__variables][]=2
-# env[__variables][0]=/^d$/
-# env[__variables][1]=/^f$/
+__rvm_file_load_env $d/.env ""
+# env[__file_env_variables][]=2
+# env[__file_env_variables][0]=/^d$/
+# env[__file_env_variables][1]=/^f$/
 
 : setting env variables with save
 rvm_saved_env=()
 unset a b
-__rvm_project_ruby_env_load_set_env "rvm_saved_env" a=1 b=2
+__rvm_set_env "rvm_saved_env" a=1 b=2
 # env[a]=/^1$/
 # env[b]=/^2$/
 # env[rvm_saved_env][]=2
@@ -43,7 +46,7 @@ __rvm_project_ruby_env_load_set_env "rvm_saved_env" a=1 b=2
 : setting env variables without save
 rvm_saved_env=()
 unset a b
-__rvm_project_ruby_env_load_set_env "" a=3 b=4
+__rvm_set_env "" a=3 b=4
 # env[a]=/^3$/
 # env[b]=/^4$/
 # env[rvm_saved_env][]=0
@@ -51,21 +54,23 @@ __rvm_project_ruby_env_load_set_env "" a=3 b=4
 : load/unload from file
 printf "a=1\nb=2\n" > $d/.env
 rvm_saved_env=( a=3 b=4 )
-__rvm_project_ruby_env_load $d/.env
+__rvm_file_load_env $d/.env
+__rvm_file_set_env
 # env[a]=/^1$/
 # env[b]=/^2$/
 # env[rvm_saved_env][]=2
 # env[rvm_saved_env][0]=/^a=3$/
 # env[rvm_saved_env][1]=/^b=4$/
-__rvm_project_ruby_env_check_unload
+__rvm_file_env_check_unload
 # env[a]=/^3$/
 # env[b]=/^4$/
 # env[rvm_saved_env][]=0
 
 : load from file no execution
 printf "a=\`uname\`\nb=\$(uname)\n" > $d/.env
-__rvm_project_ruby_env_load $d/.env
-# env[a]=/^\\`uname\\`$/
+__rvm_file_load_env $d/.env
+__rvm_file_set_env
+# env[a]=/^`uname`$/
 # env[b]=/uname/
 
 : cd + .ruby-env + ';;'
@@ -129,7 +134,7 @@ printf 'env-BLA6=\\\\\\`development\\\\\\`\n' >> $d/.versions.conf
 cd "$d"
 # match!=/bla/
 # match[stderr]!=/bla/
-# env[BLA0]=/^\\`development\\`$/
+# env[BLA0]=/^`development`$/
 # env[BLA1]=/^\\`development\\`$/
 # env[BLA2]=/^\\`development\\`$/
 # env[BLA3]=/^\\\\`development\\\\`$/
@@ -142,6 +147,35 @@ cd
 # env[BLA0]=/^test$/
 # env[rvm_saved_env][]=0
 rm $d/.versions.conf
+
+### test with enabled trusting
+unset rvm_trust_rvmrcs_flag
+
+: cd + .ruby-env + ''
+rvm_saved_env=()
+cd
+RAILS_ENV=""
+printf "2.1.1" > $d/.ruby-version
+printf "RAILS_ENV='development'" > $d/.ruby-env
+rvm rvmrc reset $d/.ruby-env
+cd "$d"
+# env[RAILS_ENV]=/^$/
+# env[rvm_saved_env][]=0
+cd
+# env[RAILS_ENV]=/^$/
+# env[rvm_saved_env][]=0
+rvm rvmrc trust $d/.ruby-env
+# match=/Marked .*\/.ruby-env as trusted/
+# match!=/Marked .*\/.rvmrc as trusted/
+cd "$d"
+# env[RAILS_ENV]=/^development$/
+# env[rvm_saved_env][]=1
+# env[rvm_saved_env][0]=/^RAILS_ENV=$/
+cd
+# env[RAILS_ENV]=/^$/
+# env[rvm_saved_env][]=0
+rvm rvmrc reset $d/.ruby-env
+rm $d/.ruby-version
 
 : cleanup
 rm -rf $d
