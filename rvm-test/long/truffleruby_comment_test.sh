79a0462dc3998f397c23e7f31395f50f744033b3
Benoit Daloze <eregontp@gmail.com>
2019-08-24 17:56:32 +0200

    Add tests for ruby -S bundle for TruffleRuby
    * See https://github.com/rvm/rvm/issues/4765

--- rvm-test/long/truffleruby_comment_test.sh
+++ rvm-test/long/truffleruby_comment_test.sh
@@ -1,6 +1,8 @@
 rvm install truffleruby # status=0; match!=/Already installed/; match=/compiling c-extensions/
 rvm truffleruby do ruby -v # status=0; match=/truffleruby/
 rvm truffleruby do rake --version # status=0; match=/rake, version/
+rvm truffleruby do ruby -S bundle --version # status=0; match=/Bundler version/
+rvm truffleruby do ruby -S bundle exec rake --version # status=0; match=/rake, version/
 rvm truffleruby do ruby -ropen-uri -e 'puts open("https://rubygems.org/") { |f| f.read(1024) }'
 # status=0; match=/RubyGems.org/
 rvm remove truffleruby # status=0; match=/removing.+truffleruby/
