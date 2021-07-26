d32b351376a6d12578ccab763c192a994c9b22a8
Benoit Daloze <eregontp@gmail.com>
2021-07-26 23:32:58 +0200

    Fix GitHub Actions CI (#5109)

--- tests/long/named_ruby_and_gemsets_comment_test.sh
+++ tests/long/named_ruby_and_gemsets_comment_test.sh
@@ -1,68 +1,68 @@
 source "$rvm_path/scripts/rvm"
 
-rvm remove  1.8.7-p374-ntest --gems
+rvm remove  2.6.5-ntest --gems
 
 ## without gemsets
 
-rvm install 1.8.7-p374-ntest --skip-gemsets --disable-binary
+rvm install 2.6.5-ntest --skip-gemsets --disable-binary
 # status=0
 # match!=/Already installed/
 # match=/Skipped importing default gemsets/
 ## match=/WARNING: Please be aware that you just installed a ruby that/
 ## match=/for a list of maintained rubies visit:/
 
-rvm install 1.8.7-p374-ntest
+rvm install 2.6.5-ntest
 # status=0; match=/Already installed/
 
-rvm 1.8.7-p374-ntest do which gem
-# match=/1.8.7-p374-ntest/
+rvm 2.6.5-ntest do which gem
+# match=/2.6.5-ntest/
 
-rvm 1.8.7-p374-ntest do gem env
+rvm 2.6.5-ntest do gem env
 
-rvm 1.8.7-p374-ntest do gem list
+rvm 2.6.5-ntest do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]!=/rubygems-bundler/
 
-rvm 1.8.7-p374-ntest do ruby -v
-# match=/1.8.7.*patchlevel 374/
+rvm 2.6.5-ntest do ruby -v
+# match=/2.6.5/
 
-rvm remove  1.8.7-p374-ntest --gems
+rvm remove  2.6.5-ntest --gems
 # status=0; match=/[Rr]emoving/
 
 
 ## default/global gemsets
 
-mkdir -p $rvm_path/gemsets/ruby/1.8.7/p374/
-printf "gem-wrappers\ntf\n"   > $rvm_path/gemsets/ruby/1.8.7/p374/global.gems
-printf "gem-wrappers\nhaml -v <5\n" > $rvm_path/gemsets/ruby/1.8.7/p374/default.gems
+mkdir -p $rvm_path/gemsets/ruby/2.6.5/
+printf "gem-wrappers\ntf\n"   > $rvm_path/gemsets/ruby/2.6.5/global.gems
+printf "gem-wrappers\nhaml -v <5\n" > $rvm_path/gemsets/ruby/2.6.5/default.gems
 
-rvm install 1.8.7-p374-ntest
+rvm install 2.6.5-ntest
 # status=0
 # match!=/Already installed/
-# match=/importing gemset .*gemsets\/ruby\/1.8.7\/p374\/global.gems/
-# match=/importing gemset .*gemsets\/ruby\/1.8.7\/p374\/default.gems/
+# match=/importing gemset .*gemsets\/ruby\/2.6.5\/global.gems/
+# match=/importing gemset .*gemsets\/ruby\/2.6.5\/default.gems/
 ## match=/WARNING: Please be aware that you just installed a ruby that/
 ## match=/for a list of maintained rubies visit:/
 
-rvm 1.8.7-p374-ntest do gem list
+rvm 2.6.5-ntest do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]=/haml/
 # match[stdout]=/tf/
 
-rvm 1.8.7-p374-ntest@global do gem list
+rvm 2.6.5-ntest@global do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]!=/haml/
 # match[stdout]=/tf/
 
 ## Cleanup
 
-rvm remove 1.8.7-p374-ntest --gems
+rvm remove 2.6.5-ntest --gems
 # status=0; match=/[Rr]emoving/
 
-rm -rf $rvm_path/gemsets/ruby/1.8.7/p374
+rm -rf $rvm_path/gemsets/ruby/2.6.5
 
-ls -1d $rvm_path/environments/*1.8.7-p374-ntest*   # status!=0
-ls -1d $rvm_path/wrappers/*1.8.7-p374-ntest*       # status!=0
-ls -1d $rvm_path/gems/*1.8.7-p374-ntest*           # status!=0
-ls -1d $rvm_path/bin/*1.8.7-p374-ntest*            # status!=0
+ls -1d $rvm_path/environments/*2.6.5-ntest*   # status!=0
+ls -1d $rvm_path/wrappers/*2.6.5-ntest*       # status!=0
+ls -1d $rvm_path/gems/*2.6.5-ntest*           # status!=0
+ls -1d $rvm_path/bin/*2.6.5-ntest*            # status!=0
 
