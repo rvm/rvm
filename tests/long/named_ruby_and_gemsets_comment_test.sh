630dc048c9fd7f89932ad42ca93c20fa8aa6fdca
Mathieu Jobin <mathieu@justbudget.com>
2023-02-15 23:12:24 +0900

    what is -ntest ?!?

--- tests/long/named_ruby_and_gemsets_comment_test.sh
+++ tests/long/named_ruby_and_gemsets_comment_test.sh
@@ -1,32 +1,32 @@
 source "$rvm_path/scripts/rvm"
 
-rvm remove  2.7.5-ntest --gems
+rvm remove  2.7.5 --gems
 
 ## without gemsets
 
-rvm install 2.7.5-ntest --skip-gemsets --disable-binary
+rvm install 2.7.5 --skip-gemsets --disable-binary
 # status=0
 # match!=/Already installed/
 # match=/Skipped importing default gemsets/
 ## match=/WARNING: Please be aware that you just installed a ruby that/
 ## match=/for a list of maintained rubies visit:/
 
-rvm install 2.7.5-ntest
+rvm install 2.7.5
 # status=0; match=/Already installed/
 
-rvm 2.7.5-ntest do which gem
-# match=/2.7.5-ntest/
+rvm 2.7.5 do which gem
+# match=/2.7.5/
 
-rvm 2.7.5-ntest do gem env
+rvm 2.7.5 do gem env
 
-rvm 2.7.5-ntest do gem list
+rvm 2.7.5 do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]!=/rubygems-bundler/
 
-rvm 2.7.5-ntest do ruby -v
+rvm 2.7.5 do ruby -v
 # match=/2.7.5/
 
-rvm remove  2.7.5-ntest --gems
+rvm remove  2.7.5 --gems
 # status=0; match=/[Rr]emoving/
 
 
@@ -36,7 +36,7 @@ mkdir -p $rvm_path/gemsets/ruby/2.7.5/
 printf "gem-wrappers\ntf\n"   > $rvm_path/gemsets/ruby/2.7.5/global.gems
 printf "gem-wrappers\nhaml -v <5\n" > $rvm_path/gemsets/ruby/2.7.5/default.gems
 
-rvm install 2.7.5-ntest
+rvm install 2.7.5
 # status=0
 # match!=/Already installed/
 # match=/importing gemset .*gemsets\/ruby\/2.7.5\/global.gems/
@@ -44,25 +44,25 @@ rvm install 2.7.5-ntest
 ## match=/WARNING: Please be aware that you just installed a ruby that/
 ## match=/for a list of maintained rubies visit:/
 
-rvm 2.7.5-ntest do gem list
+rvm 2.7.5 do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]=/haml/
 # match[stdout]=/tf/
 
-rvm 2.7.5-ntest@global do gem list
+rvm 2.7.5@global do gem list
 # match[stderr]=/\A\Z/
 # match[stdout]!=/haml/
 # match[stdout]=/tf/
 
 ## Cleanup
 
-rvm remove 2.7.5-ntest --gems
+rvm remove 2.7.5 --gems
 # status=0; match=/[Rr]emoving/
 
 rm -rf $rvm_path/gemsets/ruby/2.7.5
 
-ls -1d $rvm_path/environments/*2.7.5-ntest*   # status!=0
-ls -1d $rvm_path/wrappers/*2.7.5-ntest*       # status!=0
-ls -1d $rvm_path/gems/*2.7.5-ntest*           # status!=0
-ls -1d $rvm_path/bin/*2.7.5-ntest*            # status!=0
+ls -1d $rvm_path/environments/*2.7.5*   # status!=0
+ls -1d $rvm_path/wrappers/*2.7.5*       # status!=0
+ls -1d $rvm_path/gems/*2.7.5*           # status!=0
+ls -1d $rvm_path/bin/*2.7.5*            # status!=0
 
