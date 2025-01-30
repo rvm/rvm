a6449a0ce3f1a573022782d00857f135adc4fc51
Piotr Kuczynski <piotr.kuczynski@gmail.com>
2025-01-31 00:03:03 +0100

    fix jruby test

--- tests/jruby/jruby_comment_test.sh
+++ tests/jruby/jruby_comment_test.sh
@@ -1,3 +1,3 @@
 rvm install jruby        # status=0; match!=/Already installed/
 rvm jruby do ruby -v     # match=/jruby/; match=/OpenJDK/
-rvm remove jruby         # status=0; match=/Removing/
+rvm remove jruby         # status=0; match=/removing/
